afa5be82c0d7178c392a25f7ccad9ebb4d85f513
HunYul <129160556+HunYul@users.noreply.github.com>
2023-04-04 17:47:19 +0900

    Add files via upload
    Window API day 3th-PSA

---
+++ WIN_API_202302/WIN_API_202302/Math/RectCollider.cpp
@@ -0,0 +1,69 @@
+#include "framework.h"
+#include "RectCollider.h"
+
+RectCollider::RectCollider(Vector2 center, Vector2 size)
+: _center(center)
+, _size(size)
+, _halfSize(size * 0.5f)
+{
+	HPEN green = CreatePen(PS_SOLID, 1, GREEN);
+	HPEN red = CreatePen(PS_SOLID, 1, RED);
+	_pens.push_back(green);
+	_pens.push_back(red);
+}
+
+RectCollider::~RectCollider()
+{
+	for (auto& pen : _pens)
+	{
+		DeleteObject(pen);
+	}
+}
+
+void RectCollider::Update()
+{
+
+}
+
+void RectCollider::Render(HDC hdc)
+{
+	SelectObject(hdc, _pens[_curPenIndex]);
+
+	float left		 = _center.x - _halfSize.x;
+	float top		 = _center.y - _halfSize.y;
+	float right		 = _center.x + _halfSize.x;
+	float bottom	 = _center.y + _halfSize.y;
+
+	Rectangle(hdc, left, top, right, bottom);
+}
+
+void RectCollider::MoveCenter(const Vector2& value)
+{
+	_center += value;
+}
+
+void RectCollider::SetCenter(const Vector2& value)
+{
+	_center = value;
+}
+
+void RectCollider::SetSize(const Vector2& size)
+{
+	_size = size;
+	_halfSize = _size * 0.5f;
+}
+
+bool RectCollider::IsCollision(Vector2 pos) //  사각형과 사각형의 충돌
+{
+	if (pos.x<Left() || pos. x > Right())
+		return false;
+	if (pos.x<Top() || pos.y > Right())
+
+		return false;
+	return true;
+}
+
+bool RectCollider::IsCollision(shared_ptr<CircleCollider> other) // 원과 사각형의 충돌
+{
+	return false;
+}
---
+++ WIN_API_202302/WIN_API_202302/Scenes/PaintScene.cpp
@@ -0,0 +1,52 @@
+#include "framework.h"
+#include "PaintScene.h"
+
+PaintScene::PaintScene()
+{
+	_circle = make_shared<CircleCollider>(Vector2(300.0f, 300.0f), 50.0f);
+	_circleMouse = make_shared<CircleCollider>(Vector2(0.0f, 0.0f), 70.0f);
+
+	_rect = make_shared<RectCollider>(Vector2(500.0f, 300.0f), Vector2(100, 150));
+}
+
+PaintScene::~PaintScene()
+{
+}
+
+void PaintScene::Update()
+{
+	Vector2 lerpResult = LERP(_circleMouse->GetCenter(), mousePos, 0.1f);
+	_circleMouse->SetCenter(lerpResult);
+
+	if (_circle->IsCollision(_circleMouse))
+	{
+		_circle->SetRed();
+		_circleMouse->SetRed();
+	}
+	else
+	{
+		_circle->SetGreen();
+		_circleMouse->SetGreen();
+	}
+
+if (_rect->IsCollision(_circleMouse)) // 사각형과 원의 충돌할때 마우스의 설정
+{
+	_rect->SetRed();
+	_circleMouse->SetRed();
+}
+else
+{
+	_rect->SetGreen();
+	_circleMouse->SetGreen();
+}
+
+_circle->Update();
+_rect->Update();
+}
+
+void PaintScene::Render(HDC hdc)
+{
+	_circle->Render(hdc);
+	_circleMouse->Render(hdc);
+	_rect->Render(hdc);
+}
